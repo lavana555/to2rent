a5b836270baf032a458940a42cf70ae7fb7faded
lavana555 <timofeevniki3@gmail.com>
2020-04-20 12:13:07 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+$('.slider').slick({
+    nextArrow:'<button class="slick-arrow slick-next"><img src="../images/arrow_next.svg" alt=""></button>',
+    prevArrow:`<button class="slick-arrow slick-prev"><img src="../images/arrow_prev.svg" alt=""></button>`
+    // dots: true,
+    // infinite: true,
+    // speed: 500,
+    // fade: true,
+    // cssEase: 'linear'
+})
+});
+
+
+  
